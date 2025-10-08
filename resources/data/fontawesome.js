649036b882c22b01107d691382a1cce5fee7f06d
Myden <168192433+MdX07r@users.noreply.github.com>
2025-10-08 21:56:59 +0300

    Add files via upload

---
+++ resources/data/fontawesome.js
@@ -0,0 +1,5 @@
+const faSolid = "qrc:/fonts/fa-solid-900.otf";
+const faRegular = "qrc:/fonts/fa-regular-400.otf";
+const faBrands = "qrc:/fonts/fa-brands-400.otf";
+
+module.exports = { faSolid, faRegular, faBrands };
